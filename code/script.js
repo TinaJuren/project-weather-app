0f9873eff4a985c7ab897ad50562df032c332272
Tina <tina.juren@gmail.com>
2021-02-18 15:08:18 +0100

    added styling including a gradient background

--- code/script.js
+++ code/script.js
@@ -1,6 +1,7 @@
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=Kiruna,Sweden&units=metric&appid=3500e3bacf630605ddf92aeaab386a9e';
 const cityName = document.getElementById('city-name');
 const temperature = document.getElementById('temperature');
+const sun = document.getElementById('sun');
 const container = document.getElementById('main');
 
 console.log('API Fetch starting')
@@ -23,22 +24,19 @@ fetch(WEATHER_URL)
     const filteredForeast = data.list.filter(item => item.dt_txt.includes('12:00'))
     //temperature.innerHTML = ` ${new Date(.main.temp)}`;
 
-    container.innerHTML = `
-      <div>
+    sun.innerHTML = `
           <p>Sunrise is: ${new Date((data.city.sunrise + data.city.timezone) * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</p>
           <p>Sunset is: ${new Date((data.city.sunset + data.city.timezone) * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</p>
-      </div>
       `;
-
-    
      
     filteredForeast.forEach((weather) => {
       const temp = weather.main.temp; 
       const type = weather.weather.description;
+      
       container.innerHTML += `
-        <section class="forecast">
-          <p>${weather.weather[0].description} </p>
+        <section class="forecast">         
           <p>${(weather.main.temp).toFixed(0)}</p>
+          <p>${weather.weather[0].description}</p>
         </section>`;
     });
   });
