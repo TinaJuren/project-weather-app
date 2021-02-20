fa4b9f2aa3cdca0144997bfd7e56fc5d1c0c1a07
Tina <tina.juren@gmail.com>
2021-02-20 18:33:50 +0100

    test of animation

--- code/script.js
+++ code/script.js
@@ -4,6 +4,8 @@ const temperature = document.getElementById('temperature');
 const description = document.getElementById('description');
 const sun = document.getElementById('sun');
 const container = document.getElementById('main');
+const snow = document.getElementById('a');
+
 
 console.log('API Fetch starting')
 //fetch sends a request to our server
@@ -22,37 +24,53 @@ fetch(WEATHER_URL)
   .then((data) => {
     console.log(data);
     cityName.innerHTML = ` ${data.city.name} `;
-    // I moved the filtere
+    
     const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
     
-    filteredForecast.forEach(() => {
+    filteredForecast.forEach((data) => {
         temperature.innerHTML = ` ${(filteredForecast[0].main.temp).toFixed(0)}`;
-        //description.innerHTLM = ` ${filteredForecast.weather.description}`;
-    });
-    
-    
+        description.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"> </img>`;
+    console.log(filteredForecast[0].main.temp)
+    console.log(data.weather[0].icon)
+      });
+
     sun.innerHTML = `
-          <p>Sunrise is: ${new Date((data.city.sunrise + data.city.timezone + (new Date().getTimezoneOffset() * 60)) * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</p>
-          <p>Sunset is: ${new Date((data.city.sunset + data.city.timezone + (new Date().getTimezoneOffset() * 60)) * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</p>
-      `;
+    <p>Sunrise is: ${new Date((data.city.sunrise + data.city.timezone + (new Date().getTimezoneOffset() * 60)) * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</p>
+    <p>Sunset is: ${new Date((data.city.sunset + data.city.timezone + (new Date().getTimezoneOffset() * 60)) * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</p>
+`;
+
+const checkWeather = () => {
+  if (filteredForecast[0].main.temp === -16.98) {
+  //document.getElementById('a').style.background = "red";
+  snow.innerHTML += `
+  <div class="snowflakes" class="content"><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i></div>
+`
+} else {
+  //document.getElementById('a').style.background = "none";
+  return false;
+};
+}
+    checkWeather()
      
-    filteredForecast.forEach((weather) => {
+    filteredForecast.forEach((data) => {
       //const temp = weather.main.temp; 
       //const type = weather.weather.description;
       
+      
       container.innerHTML += `
         <section class="forecast">         
-          <p>${(weather.main.temp).toFixed(0)}</p>
-          <p>${weather.weather[0].description}</p>
+          <p>${(data.main.temp).toFixed(0)}</p>
+          <p>${data.weather[0].description}</p>
+          <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"> </img>
         </section>`;
     });
   });
 
-  fetch(WEATHER_URL)
+  /*fetch(WEATHER_URL)
   .then((response) => {
     return response.json();
   })
   .then(data => {
     
-  })
+  })*/
 console.log('API Fetch Initiation');
